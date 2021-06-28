69515c5ebfea276c1e76e74ae460308053f6092c
Gareth Evans <garethjevans@users.noreply.github.com>
2021-06-28 14:25:04 +0100

    Support dev builds

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,10 @@
 set -e
 
 # extract info
-version=${GITHUB_REF}
-version=${version#refs/tags/}
-version=${version#v}
+# version=${GITHUB_REF}
+# version=${version#refs/tags/}
+# version=${version#v}
+version=dev
 
 
 name=$(yq -r .final_name config/final.yml)
@@ -37,19 +38,12 @@ if [ -f releases/${name}/${name}-${version}.yml ]; then
 fi
 
 echo "creating bosh release: ${name}-${version}.tgz"
-cat - > config/private.yml <<EOS
----
-blobstore:
-  options:
-    access_key_id: ${AWS_BOSH_ACCES_KEY_ID}
-    secret_access_key: ${AWS_BOSH_SECRET_ACCES_KEY}
-EOS
-bosh create-release --force --final --version=${version} --tarball=${name}-${version}.tgz
+bosh create-release --force --timestamp-version --tarball=${name}-${version}.tgz
 
-echo "pushing changes to git repository"
-git add releases/${name}/${name}-${version}.yml
-git commit -a -m "cutting release ${version}"
-git push ${remote_repo} HEAD:${INPUT_TARGET_BRANCH}
+# echo "pushing changes to git repository"
+# git add releases/${name}/${name}-${version}.yml
+# git commit -a -m "cutting release ${version}"
+# git push ${remote_repo} HEAD:${INPUT_TARGET_BRANCH}
 
 # make asset readble outside docker image
 chmod 644 ${name}-${version}.tgz
